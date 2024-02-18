e92ba8b09fce7b770ddfb0cc166197cfcf7109fa
aakritil <67278558+aakritil@users.noreply.github.com>
2024-02-17 17:56:42 -0800

    i am comitting al my changes

---
+++ CardinalKit-Example/CardinalKit/Components/Tasks/Views/DataCollectionView.swift
@@ -0,0 +1,19 @@
+//
+//  DataCollectionView.swift
+//  CardinalKit_Example
+//
+//  Created by aakriti lakshmanan on 2/17/24.
+//  Copyright © 2024 CardinalKit. All rights reserved.
+//
+
+import SwiftUI
+
+struct DataCollectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DataCollectionView()
+}
---
+++ CardinalKit-Example/CardinalKit/Components/Tasks/Views/HotlineUIView.swift
@@ -0,0 +1,19 @@
+//
+//  HotlineUIView.swift
+//  CardinalKit_Example
+//
+//  Created by aakriti lakshmanan on 2/17/24.
+//  Copyright © 2024 CardinalKit. All rights reserved.
+//
+
+import SwiftUI
+
+struct HotlineUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HotlineUIView()
+}
